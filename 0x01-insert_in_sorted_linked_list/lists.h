9d06e0346beb304981469a0bfd36ecf3f1a66494
csarsound <csarsound@hotmail.com>
2022-12-15 18:33:26 -0500

    InseretInSorted

--- 0x01-insert_in_sorted_linked_list/lists.h
+++ 0x01-insert_in_sorted_linked_list/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
-#include <stdlib.h>
+
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -11,10 +11,12 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
+
 } listint_t;
 
 size_t print_listint(const listint_t *h);
